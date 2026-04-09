982cf3a645f3db902517bd7c1bb40097b1a1a3c0
Juli4ndvlp <juliandvlp01@gmail.com>
2026-04-09 14:33:37 -0500

    Correct files

---
+++ script.js
@@ -0,0 +1,11 @@
+const botones = document.querySelectorAll(".btn-agregar");
+
+
+botones.forEach(function (boton) {
+    boton.addEventListener("click", function () {
+        const nombre = boton.dataset.nombre;
+        const precio = Number(boton.dataset.precio);
+
+        console.log(nombre, precio);
+    });
+});
